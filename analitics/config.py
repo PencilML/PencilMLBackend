9ebf56fccd810f95b145d1be99e31082fc3f43dc
Artem Golovko <artem.golovko@siemens.com>
2019-12-01 00:40:23 +0300

    fix parsing boolean env var

--- analitics/config.py
+++ analitics/config.py
@@ -1,8 +1,9 @@
 import os
+from distutils.util import strtobool
 
 MODELS_DIR = os.environ.get("PENCIL_ML_ANALYTICS_MODELS_DIR")
 MODEL_NAME = os.environ.get("PENCIL_ML_ANALYTICS_MODEL_NAME", "dextr_pascal-sbd")
-USE_GPU = os.environ.get("PENCIL_ML_ANALYTICS_USE_GPU", True)
+USE_GPU = strtobool(os.environ.get("PENCIL_ML_ANALYTICS_USE_GPU", "True"))
 PAD = os.environ.get("PENCIL_ML_ANALYTICS_PAD", 50)
 THRESHOLD = os.environ.get("PENCIL_ML_ANALYTICS_THRESHOLD", 0.8)
 GPU_ID = os.environ.get("PENCIL_ML_ANALYTICS_THRESHOLD", 0)
