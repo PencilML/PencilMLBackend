afcd44d6f19c881a8c18840edc57f467df7dd619
Artem Golovko <artem.golovko@siemens.com>
2019-11-30 12:16:27 +0300

    make configure from env variables

--- analitics/config.py
+++ analitics/config.py
@@ -1,6 +1,8 @@
-MODELS_DIR = "C://Users//scrat98//Documents//github//PencilMLBackend//analitics//models"
-MODEL_NAME = "dextr_pascal-sbd"
-USE_GPU = True
-PAD = 50
-THRESHOLD = 0.8
-GPU_ID = 0
+import os
+
+MODELS_DIR = os.environ.get("PENCIL_ML_ANALYTICS_MODELS_DIR")
+MODEL_NAME = os.environ.get("PENCIL_ML_ANALYTICS_MODEL_NAME", "dextr_pascal-sbd")
+USE_GPU = os.environ.get("PENCIL_ML_ANALYTICS_USE_GPU", True)
+PAD = os.environ.get("PENCIL_ML_ANALYTICS_PAD", 50)
+THRESHOLD = os.environ.get("PENCIL_ML_ANALYTICS_THRESHOLD", 0.8)
+GPU_ID = os.environ.get("PENCIL_ML_ANALYTICS_THRESHOLD", 0)
--- web/config.py
+++ web/config.py
@@ -1,11 +1,11 @@
 # flask config
 import os
 
-DEBUG = True  # Turns on debugging features in Flask
+DEBUG = os.environ.get("PENCIL_ML_WEB_DEBUG", True)  # Turns on debugging features in Flask
 
 # app config
 DEXTR_IMAGE_FORM_DATA_KEY = "image"
-DEXTR_IMAGE_UPLOAD_FOLDER = 'C://Users//scrat98//Documents//github//PencilMLBackend//web//upload'
+DEXTR_IMAGE_UPLOAD_FOLDER = os.environ.get("PENCIL_ML_WEB_DEXTR_IMAGE_UPLOAD_FOLDER")
 DEXTR_IMAGE_ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 if not os.path.exists(DEXTR_IMAGE_UPLOAD_FOLDER):
